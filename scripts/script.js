da779c2d9d1c74614db237369fc4dba9d5783879
Anna Tuban <mshannahstarling@gmail.com>
2021-11-14 15:33:49 +0300

    fix: add form.reset and replace validation action on validate.js

--- scripts/script.js
+++ scripts/script.js
@@ -28,37 +28,33 @@ const initialCards = [
   { name: 'Венеция', link: './images/elements/venezia.jpg' },
 ];
 
-function renderCard() {
-  initialCards.forEach((item) => {
-    createCard(item.name, item.link);
-  });
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
+  window.removeEventListener('keydown', closePopupOnEsc);
 }
-
-renderCard();
 
 function openPopup(popup) {
   popup.classList.add('popup_opened');
   window.addEventListener('keydown', closePopupOnEsc);
 }
 
-function closePopupOnEsc(e) {
-  if (e.key === 'Escape') {
+function closePopupOnEsc(evt) {
+  if (evt.key === 'Escape') {
     const popup = document.querySelector('.popup_opened');
+    const form = popup.querySelector('.popup__form');
     closePopup(popup);
+    if (form !== null) form.reset();
   }
 }
 
 function closePopupOnOverlay(evt) {
-  const popup = document.querySelector('.popup_opened');
-  if (evt.target !== evt.currentTarget) {
+  const popup = evt.target;
+  const form = popup.querySelector('.popup__form');
+  if (popup !== evt.currentTarget) {
     return;
   }
   closePopup(popup);
-}
-
-function closePopup(popup) {
-  popup.classList.remove('popup_opened');
-  window.removeEventListener('keydown', closePopupOnEsc);
+  if (form !== null) form.reset();
 }
 
 function editProfile(evt) {
@@ -68,14 +64,6 @@ function editProfile(evt) {
   closePopup(popupProfile);
 }
 
-function addCard(evt) {
-  evt.preventDefault();
-
-  createCard(title.value, photo.value);
-  newCardForm.reset();
-  closePopup(popupNewCard);
-}
-
 function getCard(cardTitleValue, cardLinkValue) {
   const cardTemplate = document.querySelector('.card-template').content;
   const card = cardTemplate.querySelector('.elements__item').cloneNode(true);
@@ -83,7 +71,7 @@ function getCard(cardTitleValue, cardLinkValue) {
 
   card.querySelector('.elements__title').textContent = cardTitleValue;
   image.src = cardLinkValue;
-  image.alt = cardTitleValue + '.';
+  image.alt = cardTitleValue + '.'; // точка в конце alt для ридеров, которые делают на ней голосовую паузу
 
   const deleteButton = card.querySelector('.elements__delete-btn');
   deleteButton.addEventListener('click', function (evt) {
@@ -108,44 +96,44 @@ function createCard(cardTitleValue, cardLinkValue) {
   cardsContainer.prepend(card);
 }
 
-function resetFormOnClosing(form) {
-  const inputs = [...form.querySelectorAll(settings.inputSelector)];
-  inputs.forEach((input) => {
-    input.classList.remove(settings.inputErrorClass);
-    const error = searchInputError(form, input);
-    error.classList.remove(settings.errorClass);
+function renderCard() {
+  initialCards.forEach((item) => {
+    createCard(item.name, item.link);
   });
-  form.reset();
 }
 
-function validateOnOpening(
-  form,
-  popup,
-  { submitButtonSelector, inactiveButtonClass }
-) {
-  toggleButton(form, submitButtonSelector, inactiveButtonClass);
-  openPopup(popup);
+renderCard();
+
+function addCard(evt) {
+  evt.preventDefault();
+
+  createCard(title.value, photo.value);
+  newCardForm.reset();
+  closePopup(popupNewCard);
 }
 
 editButton.addEventListener('click', (evt) => {
-  console.log(evt.target);
   nameInput.value = profileName.textContent;
   aboutInput.value = profileDescription.textContent;
-  validateOnOpening(profileForm, popupProfile, settings);
+  //кнопка редактирования при открытии всегда валидна
+  toggleButton(profileForm, settings);
+  resetError(profileForm, settings);
+  openPopup(popupProfile);
 });
 
 addButton.addEventListener('click', () => {
-  validateOnOpening(newCardForm, popupNewCard, settings);
+  resetError(newCardForm, settings);
+  openPopup(popupNewCard);
 });
 
 closeBtnProfile.addEventListener('click', () => {
   closePopup(popupProfile);
-  resetFormOnClosing(profileForm);
+  profileForm.reset();
 });
 
 closeBtnNewCard.addEventListener('click', () => {
   closePopup(popupNewCard);
-  resetFormOnClosing(newCardForm);
+  newCardForm.reset();
 });
 closeBtnImage.addEventListener('click', () => closePopup(popupImage));
 popups.forEach((popup) => popup.addEventListener('click', closePopupOnOverlay));
