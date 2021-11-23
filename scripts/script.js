696e6e4e0abab8dbc69053f3af2962a011e22e1f
Anna Tuban <mshannahstarling@gmail.com>
2021-11-23 17:43:39 +0300

    delete: data (constants)

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,3 @@
-const popupProfile = document.querySelector('.popup_type_edit-profile');
-const popupNewCard = document.querySelector('.popup_type_add-card');
-const popupImage = document.querySelector('.popup_type_image');
-const profile = document.querySelector('.profile');
-const cardsContainer = document.querySelector('.elements__list');
-const profileForm = document.forms.user;
-const newCardForm = document.forms.card;
-const nameInput = profileForm.querySelector('.popup__item_el_name');
-const aboutInput = profileForm.querySelector('.popup__item_el_description');
-const profileName = profile.querySelector('.profile__name');
-const profileDescription = profile.querySelector('.profile__description');
-const editButton = profile.querySelector('.profile__btn_action_edit');
-const addButton = profile.querySelector('.profile__btn_action_add');
-const closeBtnProfile = popupProfile.querySelector('.popup__btn_action_close');
-const closeBtnNewCard = popupNewCard.querySelector('.popup__btn_action_close');
-const picture = popupImage.querySelector('.popup__image');
-const pictureCaption = popupImage.querySelector('.popup__caption');
-const closeBtnImage = popupImage.querySelector('.popup__btn_action_close');
-const title = popupNewCard.querySelector('.popup__item_el_title');
-const photo = popupNewCard.querySelector('.popup__item_el_link');
-const popups = [...document.querySelectorAll('.popup')];
-const initialCards = [
-  { name: 'Мыс Марлера', link: './images/elements/croatia.jpg' },
-  { name: 'Лекко', link: './images/elements/lecco.jpg' },
-  { name: 'Москва', link: './images/elements/moscow.jpg' },
-  { name: 'Пиза', link: './images/elements/pisa.jpg' },
-  { name: 'Ватикан', link: './images/elements/roma.jpg' },
-  { name: 'Венеция', link: './images/elements/venezia.jpg' },
-];
-
 function closePopup(popup) {
   popup.classList.remove('popup_opened');
   window.removeEventListener('keydown', closePopupOnEsc);
