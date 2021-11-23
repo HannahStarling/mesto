c227f93b786ea0c62587aeb277f1d31c7820d4c1
Anna Tuban <mshannahstarling@gmail.com>
2021-11-23 18:03:58 +0300

    feat: refactor addCard action

--- scripts/script.js
+++ scripts/script.js
@@ -34,58 +34,30 @@ function editProfile(evt) {
   closePopup(popupProfile);
 }
 
-function getCard(cardTitleValue, cardLinkValue) {
-  const cardTemplate = document.querySelector('.card-template').content;
-  const card = cardTemplate.querySelector('.elements__item').cloneNode(true);
-  const image = card.querySelector('.elements__image');
+function renderCard(data) {
+  const card = new Card(data, '.card-template').createCard();
 
-  card.querySelector('.elements__title').textContent = cardTitleValue;
-  image.src = cardLinkValue;
-  image.alt = cardTitleValue + '.'; // точка в конце alt для ридеров, которые делают на ней голосовую паузу
-
-  const deleteButton = card.querySelector('.elements__delete-btn');
-  deleteButton.addEventListener('click', function (evt) {
-    evt.target.closest('.elements__item').remove();
-  });
-
-  const likeButton = card.querySelector('.elements__like-btn');
-  likeButton.addEventListener('click', function (evt) {
-    evt.target.classList.toggle('elements__like-btn_active');
-  });
-
-  image.addEventListener('click', () => {
-    picture.src = cardLinkValue;
-    pictureCaption.textContent = cardTitleValue;
-    openPopup(popupImage);
-  });
-  return card;
-}
-
-function createCard(cardTitleValue, cardLinkValue) {
-  const card = getCard(cardTitleValue, cardLinkValue);
   cardsContainer.prepend(card);
 }
 
-function renderCard() {
-  initialCards.forEach((initialCard) => {
-    const card = new Card(initialCard, '.card-template').createCard();
-
-    cardsContainer.prepend(card);
-  });
-}
-
-renderCard();
+initialCards.forEach((initialCard) => {
+  renderCard(initialCard);
+});
 
 function addCard(evt) {
   evt.preventDefault();
+  const data = {
+    name: title.value,
+    link: photo.value,
+  };
 
-  createCard(title.value, photo.value);
+  renderCard(data);
   newCardForm.reset();
   toggleButton(newCardForm, settings);
   closePopup(popupNewCard);
 }
 
-editButton.addEventListener('click', (evt) => {
+editButton.addEventListener('click', () => {
   nameInput.value = profileName.textContent;
   aboutInput.value = profileDescription.textContent;
   //кнопка редактирования при открытии всегда валидна
