67023585cda5477ae962eb9d6efc1a1785cf6f76
Anna Tuban <mshannahstarling@gmail.com>
2021-11-24 14:05:27 +0300

    feat: add open/close action modules

---
+++ scripts/utils.js
@@ -0,0 +1,28 @@
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
+  window.addEventListener('keydown', closePopupOnEsc);
+}
+
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
+  window.removeEventListener('keydown', closePopupOnEsc);
+}
+
+function closePopupOnEsc(evt) {
+  if (evt.key === 'Escape') {
+    const popup = document.querySelector('.popup_opened');
+    const form = popup.querySelector('.popup__form');
+    closePopup(popup);
+    if (form !== null) form.reset();
+  }
+}
+
+function closePopupOnOverlay(evt) {
+  const popup = evt.target;
+  const form = popup.querySelector('.popup__form');
+  if (popup !== evt.currentTarget) {
+    return;
+  }
+  closePopup(popup);
+  if (form !== null) form.reset();
+}
