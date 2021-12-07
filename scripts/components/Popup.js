7ddb07e0b3dda4295597324c9f60f6870f8a6b64
Anna Tuban <mshannahstarling@gmail.com>
2021-12-07 20:11:40 +0300

    fix: change anonimys func in removeEventListner, bind this

--- scripts/components/Popup.js
+++ scripts/components/Popup.js
@@ -1,16 +1,17 @@
 export default class Popup {
   constructor(popupSelector) {
     this._popup = document.querySelector(popupSelector);
+    this._handleEscClose = this._handleEscClose.bind(this);
   }
 
   open() {
     this._popup.classList.add('popup_opened');
-    window.addEventListener('keydown', (evt) => this._handleEscClose(evt));
+    window.addEventListener('keydown', this._handleEscClose);
   }
 
   close() {
     this._popup.classList.remove('popup_opened');
-    window.removeEventListener('keydown', (evt) => this._handleEscClose(evt));
+    window.removeEventListener('keydown', this._handleEscClose);
   }
 
   _handleEscClose(evt) {
