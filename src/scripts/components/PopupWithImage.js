674fbebd625cf9c84947d814755ca339c8540cbf
Anna Tuban <mshannahstarling@gmail.com>
2021-12-09 00:26:14 +0300

    fix: PopupWithImage class querySelector

--- src/scripts/components/PopupWithImage.js
+++ src/scripts/components/PopupWithImage.js
@@ -2,13 +2,14 @@ import Popup from './Popup.js';
 export default class PopupWithImage extends Popup {
   constructor(popupSelector) {
     super(popupSelector);
+    this._picture = this._popup.querySelector('.popup__image');
+    this._caption = this._popup.querySelector('.popup__caption');
   }
 
   open({ title, link }) {
     super.open();
-    this._picture = this._popup.querySelector('.popup__image');
     this._picture.src = link;
     this._picture.alt = title;
-    this._popup.querySelector('.popup__caption').textContent = title;
+    this._caption.textContent = title;
   }
 }
